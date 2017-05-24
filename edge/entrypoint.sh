99e96e948ef69219ab0d5ff2b2ab330f04b0ed48
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2017-05-24 13:51:03 -0400

    Add default config support, link support, fix entrypoint.sh

--- edge/entrypoint.sh
+++ edge/entrypoint.sh
@@ -1,6 +1,35 @@
 #!/bin/sh
+if [ ! -f /databases.ini ]; then
+    echo "/databases.ini doesn't exist"
+
+    if [ -z ${POSTGRES_PORT_5432_TCP_ADDR} ]; then
+        env
+        echo "POSTGRES_PORT_5432_TCP_ADDR is not set - can't generate default databases.ini"
+        exit 1
+    fi
+
+    printf "\
+[databases]
+* = host=${POSTGRES_PORT_5432_TCP_ADDR} port=${POSTGRES_PORT_5432_TCP_PORT}
+" > /databases.ini
+
+    echo -e "POSTGRES_PORT_5432_TCP_ADDR is set - generated default databases.ini pooling all connections to any DB\n"
+fi
+
+if [ ! -f /pg_hba ]; then
+    echo "/pg_hba doesn't exist'"
+
+    printf "\
+local       all     all     peer
+host        all     all     0.0.0.0/0       md5
+" > /pg_hba
+
+    echo -e "Generated default pg_hba allowing peer authenticated local (socket) connections and password md5 authenticated remote connections from any host to any database\n"
+fi
+
+echo "Starting pgbouncer..."
 if [ -n "$PG_USER" ]; then
-    /usr/bin/pgbouncer -u ${PG_USER} /pgbouncer.ini
+    exec /usr/bin/pgbouncer -u $PG_USER /pgbouncer.ini
 else
-    /usr/bin/pgbouncer /pgbouncer.ini
+    exec /usr/bin/pgbouncer -u postgres /pgbouncer.ini
 fi
